7c7c70eefa26f4fd1e29fff706348024e61bb2de
Lee Rhodes <lrhodes@yahoo-inc.com>
2016-09-05 21:13:43 -0700

    Fix broken link

--- src/main/java/com/yahoo/sketches/hive/theta/package-info.java
+++ src/main/java/com/yahoo/sketches/hive/theta/package-info.java
@@ -5,7 +5,7 @@
 
 /**
  * <p>The theta package contains all the sketch classes that are members of the 
- * <a href="{@docRoot}/resources/dictionary.html#thetaSketch">Theta Sketch Framework</a>.  
+ * <a href="https://github.com/DataSketches/DataSketches.github.io/blob/master/docs/pdf/ThetaSketchFramework.pdf">Theta Sketch Framework</a>.  
  * The basic sketching functionality in this package is also 
  * accessible from Hadoop Pig UDFs found in the <i>sketches-pig</i> repository, 
  * and from Hadoop Hive UADFs and UDFs found in the <i>sketches-hive</i> repository.
