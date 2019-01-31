d891a59a6f16cdc82744f8bc0a7fb16b07a0d4af
saydakov <saydakov@yahoo-inc.com>
2019-01-31 12:43:50 -0800

    quantiles sketch to string UDFs

---
+++ src/test/java/com/yahoo/sketches/hive/quantiles/DoublesSektchToStringUDFTest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2019, Verizon Media.
+ * Licensed under the terms of the Apache License 2.0. See LICENSE file at the project root for terms.
+ */
+
+package com.yahoo.sketches.hive.quantiles;
+
+import com.yahoo.sketches.quantiles.DoublesSketch;
+
+import org.testng.annotations.Test;
+import org.apache.hadoop.io.BytesWritable;
+import org.testng.Assert;
+
+public class DoublesSektchToStringUDFTest {
+
+  @Test
+  public void nullSketch() {
+    final String result = new DoublesSketchToStringUDF().evaluate(null);
+    Assert.assertNull(result);
+  }
+
+  @Test
+  public void normalCase() {
+    final DoublesSketch sketch = DoublesSketch.builder().build();
+    final String result = new DoublesSketchToStringUDF().evaluate(new BytesWritable(sketch.toByteArray()));
+    Assert.assertNotNull(result);
+    Assert.assertTrue(result.length() > 0);
+  }
+
+}
